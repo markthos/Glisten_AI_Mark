6e216fa8cc33f0e483e7c53fdda06a48c4c382a8
markthos <aydenthos@gmail.com>
2024-03-19 17:15:13 -0700

    settings slice configured and tailwind added

--- wedloc/src/app/layout.tsx
+++ wedloc/src/app/layout.tsx
@@ -1,15 +1,26 @@
+import "./globals.css";
+import { DM_Sans } from "next/font/google";
+
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-dm-sans",
+});
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-      <PrismicPreview repositoryName={repositoryName} />
+    <html lang="en" className={dmSans.variable}>
+      <body className="grid bg-[#070815] text-white">
+        <main>{children}</main>
+        <PrismicPreview repositoryName={repositoryName} />
+      </body>
     </html>
   );
 }

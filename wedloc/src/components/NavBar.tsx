39c17a8a4cc63b4555434d0e5d2fbc2f33057158
markthos <aydenthos@gmail.com>
2024-03-26 11:02:06 -0700

    header and button work done

--- wedloc/src/components/NavBar.tsx
+++ wedloc/src/components/NavBar.tsx
@@ -1,31 +1,66 @@
 "use client";
 
-import WordMark from "@/components/WordMark"
-import { Content } from "@prismicio/client";
-import { PrismicNextLink } from "@prismicio/next";
+import { useState } from "react";
 import Link from "next/link";
+import { Content, asLink } from "@prismicio/client";
+import { PrismicNextLink } from "@prismicio/next";
+import ButtonLink from "@/components/ButtonLink";
+import WordMark from "@/components/WordMark";
+import { usePathname } from "next/navigation";
 
 type NavBarProps = {
     settings: Content.SettingsDocument;
 };
 
 export default function NavBar( { settings }: NavBarProps) {
-  return (
+    const [open, setOpen] = useState(false);
+    const pathname  = usePathname(); 
+  
+  
+    return (
+  
     <nav className="px-4 py-4 md:px-6 md-:py-6" aria-laberl="Main">
-        <div className="mx-auto flex max-w-6xl flex-=col justify-between py-2 font-medium text-white md:flex-row md:items-center">
+        <div className="mx-auto max-w-6xl flex-=col justify-between py-2 font-medium text-white md:flex-row md:items-center">
             <Link href="/">
             <WordMark />
             <span className="sr-only">Wedloc</span>
             </Link>
 
             <ul className="flex gap-6">
-                {settings.data.navigation.map((item) => (
-                    <li key={item.label}>
-                        <PrismicNextLink field={item.link} className="inline-flex min-h-11 items-center">
+                {settings.data.navigation.map((item) => {
+                    if (item.cta_button) {
+                        return (
+                            <ButtonLink
+                            key={item.label}
+                            field={item.link}
+                            onClick={() => setOpen(false)}
+                            aria-current={
+                              pathname.includes(asLink(item.link) as string)
+                                ? "page"
+                                : undefined
+                            }
+                          >
                             {item.label}
-                        </PrismicNextLink>
-                    </li>
-                ))}
+                          </ButtonLink>
+                        );
+                    }
+                
+                    return (
+                        <li key={item.label}>
+                          <PrismicNextLink
+                            field={item.link}
+                            className="inline-flex min-h-11 items-center"
+                            aria-current={
+                              pathname.includes(asLink(item.link) as string)
+                                ? "page"
+                                : undefined
+                            }
+                          >
+                            {item.label}
+                          </PrismicNextLink>
+                        </li>
+                    );
+                })}
             </ul>
         </div>
     </nav>

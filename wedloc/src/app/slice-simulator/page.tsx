e9bbfcf16a884722e41ca4114f2f19515ad5e159
markthos <aydenthos@gmail.com>
2024-03-26 14:06:19 -0700

    setup hero slice and styled

--- wedloc/src/app/slice-simulator/page.tsx
+++ wedloc/src/app/slice-simulator/page.tsx
@@ -1,20 +1,43 @@
-import {
-  SliceSimulator,
-  SliceSimulatorParams,
-  getSlices,
-} from "@slicemachine/adapter-next/simulator";
+
 import { SliceZone } from "@prismicio/react";
+import * as prismic from "@prismicio/client";
 
+import { createClient } from "@/prismicio";
 import { components } from "@/slices";
+import { Metadata } from "next";
 
-export default function SliceSimulatorPage({
-  searchParams,
-}: SliceSimulatorParams) {
-  const slices = getSlices(searchParams.state);
+/**
+ * This component renders your homepage.
+ *
+ * Use Next's generateMetadata function to render page metadata.
+ *
+ * Use the SliceZone to render the content of the page.
+ */
 
-  return (
-    <SliceSimulator>
-      <SliceZone slices={slices} components={components} />
-    </SliceSimulator>
-  );
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const home = await client.getByUID("page", "home");
+
+  return {
+    title: prismic.asText(home.data.title),
+    description: home.data.meta_description,
+    openGraph: {
+      title: home.data.meta_title || undefined,
+      images: [
+        {
+          url: home.data.meta_image.url || "",
+        },
+      ],
+    },
+  };
+}
+
+export default async function Index() {
+  /**
+   * The client queries content from the Prismic API
+   */
+  const client = createClient();
+  const home = await client.getByUID("page", "home");
+
+  return <SliceZone slices={home.data.slices} components={components} />;
 }
--- wedloc/src/components/NavBar.tsx
+++ wedloc/src/components/NavBar.tsx
@@ -19,8 +19,8 @@ export default function NavBar( { settings }: NavBarProps) {
   
     return (
   
-    <nav className="px-4 py-4 md:px-6 md-:py-6" aria-laberl="Main">
-        <div className="mx-auto max-w-6xl flex-=col justify-between py-2 font-medium text-white md:flex-row md:items-center">
+      <nav className="md-:py-6 px-4 py-4 md:px-6" aria-label="Main">
+        <div className="mx-auto flex max-w-6xl flex-col justify-between py-2 font-medium text-white md:flex-row md:items-center">
             <Link href="/">
             <WordMark />
             <span className="sr-only">Wedloc</span>

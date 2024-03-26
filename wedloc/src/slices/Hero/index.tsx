c9376309ad91e37251907e381e36186b21919b7f
markthos <aydenthos@gmail.com>
2024-03-26 14:10:13 -0700

    minor change

--- wedloc/src/slices/Hero/index.tsx
+++ wedloc/src/slices/Hero/index.tsx
@@ -40,7 +40,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       )}
       {isFilled.image(slice.primary.image) && (
         <div className="glass-container mt-16 w-fit">
-          <div className="absolute inset-0 -z-10 bg-blue-500/30 blur-2xl filter" />
+          <div className="absolute inset-0 -z-10 bg-blue-500/30 blur-2xl filter"/>
           <PrismicNextImage 
             className="rounded-lg" 
             field={slice.primary.image} />
